a0712f1a10421c6e80548cdfd6b290b8b82ce15c
zhaoyushu <zhaoyushu02@meituan.com>
2026-07-06 17:30:20 +0800

    fix：修复手机端显示问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/noto-serif-sc/400.css'
+import '@fontsource/noto-serif-sc/500.css'
 import './index.css'
 import App from './App.tsx'
 
